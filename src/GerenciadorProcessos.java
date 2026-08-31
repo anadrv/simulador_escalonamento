3707414ef3f54347c7407baf61bd890e3d2d16dd
lavi <sloowlaah1@gmail.com>
2026-08-31 17:36:10 -0300

    feat: implementar gerenciador de processos

--- src/GerenciadorProcessos.java
+++ src/GerenciadorProcessos.java
@@ -1,2 +1,21 @@
+import java.util.Random;
+
 public class GerenciadorProcessos {
+
+    private int proximoId = 1;
+    private Random random = new Random();
+
+    public Processo gerarProcesso() {
+
+        int quantidadeInstrucoes = random.nextInt(41) + 10;
+
+        Processo processo = new Processo(
+                proximoId,
+                quantidadeInstrucoes
+        );
+
+        proximoId++;
+
+        return processo;
+    }
 }
