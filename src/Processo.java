fd4e57c8432900fa87efb225de7564fe8905ee4a
juliaf006 <jfialhodelima@gmail.com>
2026-08-31 16:54:34 -0300

    Processo ok

--- src/Processo.java
+++ src/Processo.java
@@ -1,2 +1,29 @@
 public class Processo {
+
+    private int id;
+    private int quantidadeInstrucoes;
+
+    public Processo(int id, int quantidadeInstrucoes) {
+        this.id = id;
+        this.quantidadeInstrucoes = quantidadeInstrucoes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantidadeInstrucoes() {
+        return quantidadeInstrucoes;
+    }
+
+    public void executar() {
+        if (quantidadeInstrucoes > 0) {
+            quantidadeInstrucoes--;
+        }
+    }
+
+    public boolean isFinalizado() {
+        return quantidadeInstrucoes == 0;
+    }
+
 }
