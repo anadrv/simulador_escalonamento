f06d4768653afbea4d63cf36bbac9ef9ca5d393e
ash <malubystailsreal@gmail.com>
2026-09-01 09:36:09 -0300

    feat: AlgoritmoEscalonamento e Escalonador.

--- src/AlgoritmoEscalonamento.java
+++ src/AlgoritmoEscalonamento.java
@@ -1,2 +1,53 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Queue;
+
 public class AlgoritmoEscalonamento {
+
+    public Processo escolherProcesso(
+            Queue<Processo> fila,
+            Escalonador.Algoritmo tipo
+    ) {
+
+        if (fila.isEmpty()) {
+            return null;
+        }
+
+        switch (tipo) {
+
+            case FCFS:
+                return fcfs(fila);
+
+            case SJF:
+                return sjf(fila);
+
+            case ROUND_ROBIN:
+                return roundRobin(fila);
+
+            default:
+                return null;
+        }
+    }
+
+    private Processo fcfs(Queue<Processo> fila) {
+        return fila.poll();
+    }
+
+    private Processo sjf(Queue<Processo> fila) {
+
+        Processo menor = Collections.min(
+                fila,
+                Comparator.comparingInt(
+                        Processo::getQuantidadeInstrucoes
+                )
+        );
+
+        fila.remove(menor);
+
+        return menor;
+    }
+
+    private Processo roundRobin(Queue<Processo> fila) {
+        return fila.poll();
+    }
 }
--- src/Escalonador.java
+++ src/Escalonador.java
@@ -1,7 +1,7 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Escalonador {
-
 
     public enum Algoritmo {
         FCFS,
@@ -9,94 +9,21 @@ public class Escalonador {
         ROUND_ROBIN
     }
 
-    private Algoritmo algoritmo;
-
-    private int quantum = 2;
-
     private Queue<Processo> filaProntos;
 
-    public Escalonador(Algoritmo algoritmo) {
-        this.algoritmo = algoritmo;
-        this.filaProntos = new LinkedList<>();
+    public Escalonador() {
+        filaProntos = new LinkedList<>();
     }
 
     public void adicionarProcesso(Processo processo) {
         filaProntos.add(processo);
     }
 
-    public Processo proximoProcesso() {
-
-        if (filaProntos.isEmpty()) {
-            return null;
-        }
-
-        switch (algoritmo) {
-
-            case FCFS:
-                return executarFCFS();
-
-            case SJF:
-                return executarSJF();
-
-            case ROUND_ROBIN:
-                return executarRoundRobin();
-
-            default:
-                return null;
-        }
+    public Queue<Processo> getFilaProntos() {
+        return filaProntos;
     }
 
-    private Processo executarFCFS() {
-        return filaProntos.poll();
-    }
-
-    private Processo executarSJF() {
-
-        Processo menor = Collections.min(
-                filaProntos,
-                Comparator.comparingInt(Processo::getTempoExecucao)
-        );
-
-        filaProntos.remove(menor);
-
-        return menor;
-    }
-
-    private Processo executarRoundRobin() {
-
-        Processo processo = filaProntos.poll();
-
-        if (processo != null) {
-            processo.executar(quantum);
-
-            if (!processo.terminou()) {
-                filaProntos.add(processo);
-            }
-        }
-
-        return processo;
-    }
-
-    public int getQuantum() {
-        return quantum;
-    }
-
-    public void setQuantum(int quantum) {
-
-        if (quantum <= 0) {
-            throw new IllegalArgumentException(
-                    "O quantum deve ser maior que zero."
-            );
-        }
-
-        this.quantum = quantum;
-    }
-
-    public Algoritmo getAlgoritmo() {
-        return algoritmo;
-    }
-
-    public void setAlgoritmo(Algoritmo algoritmo) {
-        this.algoritmo = algoritmo;
+    public boolean possuiProcessos() {
+        return !filaProntos.isEmpty();
     }
 }
